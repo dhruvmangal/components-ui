ecb1274aef05fb35319107ab55318c5f5394b03f
Ashish Patel <ashish.patel@hiteshi.com>
2023-04-05 17:19:26 +0530

    static header

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,22 +1,35 @@
-import { Header } from './Header';
+import { Header } from "./Header";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 };
 
 export const LoggedIn = {
   args: {
     user: {
-      name: 'Jane Doe',
+      name: "Jane Doe",
     },
+    HeaderTabs: [
+      "Home",
+      "Company",
+      "Marketplace",
+      "Features",
+      "Team",
+      "Contact",
+    ],
   },
 };
 
-export const LoggedOut = {};
+export const LoggedOut = {
+  args: {
+    HeaderTabs: [" Home"],
+  },
+};
+export const sub = {};
